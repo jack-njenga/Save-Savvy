21ea3c6fc99faa45b40153beb7b7c10e36ffa5f2
jack-njenga <jacknjenga99@gmail.com>
2023-11-08 00:48:33 +0300

    update on landing page

--- backend/api/v1/app.py
+++ backend/api/v1/app.py
@@ -5,9 +5,6 @@
 import sys
 sys.path.extend([".", "../", "../../", "../../../", "../../../../"])
 
-print(sys.executable)
-print(sys.path)
-
 # from models import *
 from os import getenv
 from flask import Flask, render_template, make_response, jsonify
